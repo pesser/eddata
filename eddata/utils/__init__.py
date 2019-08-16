edd7753abcb4433ffd4cb18dfa39560af6e3b12e
supermario94123 <supermario94123@gmail.com>
2019-08-16 16:25:06 +0200

    removed edflow dependency

--- eddata/utils/__init__.py
+++ eddata/utils/__init__.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from edflow.iterators.batches import load_image, DatasetMixin, resize_float32, save_image
 import numpy as np
 from tqdm import tqdm
 import urllib
